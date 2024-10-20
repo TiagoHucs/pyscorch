12163e78e2381e08dd41184c8dea31d425e1be90
Tiago S. Hucs <tiagohucs@gmail.com>
2024-10-20 10:58:50 -0300

    cannon configs

--- tank.py
+++ tank.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from settings import *
 from projectile import *
 
@@ -15,9 +16,27 @@ class Tank:
 
     def draw(self, screen):
         # Desenhar o corpo do tanque
-        pygame.draw.rect(screen, (255, 0, 0), [self.x, self.y, self.width, self.height])
+        pygame.draw.rect(screen, RED, [self.x, self.y, self.width, self.height])
+        
+        
+        # Tamanho da linha e ângulo (em graus)
+        canon_size = 20
+        canon_diam = 5
+
+        # Converter o ângulo para radianos
+        angle_radians = math.radians(self.angle)
+
+        # Calcular as coordenadas finais com base no ângulo e no comprimento da linha
+        # Ponto inicial da linha
+        start_pos = (self.x + (self.width // 2), self.y)
+        
+        end_pos = (
+            start_pos[0] + canon_size * math.cos(angle_radians), 
+            start_pos[1] - canon_size * math.sin(angle_radians)  # O eixo Y cresce para baixo no Pygame
+        )
+        
         # Desenhar o canhão
-        # pygame.draw.rect(screen, (0, 0, 0), [self.x + 10, self.y - 10, 20, 10])
+        pygame.draw.line(screen, RED, start_pos, end_pos, canon_diam)
 
     def move(self, direction):
         if direction == "LEFT" and self.x > 0:
